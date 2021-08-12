7c22f117ee31506e64739cc105dd397da74c3eff
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2021-08-12 18:19:29 +0200

    Updated constructor to class Launcher, added options button as well as a subclass SettingsLauncher

--- src/theapp/core/App.java
+++ src/theapp/core/App.java
@@ -118,7 +118,7 @@ public class App extends Canvas implements Runnable{
     }
     
     public static void main(String[] args){
-        Launcher app = new Launcher();
+        Launcher app = new Launcher("Game Launcher", 500, 500);
         return;
     }
 }
--- src/theapp/core/Launcher.java
+++ src/theapp/core/Launcher.java
@@ -7,23 +7,32 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 public class Launcher extends JFrame {
-    private JPanel window = new JPanel();
-    private JButton btnPlay;
-
-    private int width = 500;
-    private int height = 500;
-    public Launcher (){
-        setTitle("Game Launcher");
+    static {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    protected JPanel window = new JPanel();
+    protected int width;
+    protected int height;
+    public Launcher (String title, int width, int height){
+        setTitle(title);
+        this.width = width;
+        this.height = height;
         setSize(new Dimension(width, height));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
         window.setLayout(null);
         getContentPane().add(window);
+        createComponents();
         setVisible(true);
-
-        btnPlay = new JButton("Play");
-        btnPlay.setBounds(width/2 - 40, height/2 - 20, 80, 40);
+    }
+    protected void createComponents(){
+        JButton btnPlay = new JButton("Play");
+        btnPlay.setBounds(width/2 - 40, height/2 - 40 -30, 80, 40);
         btnPlay.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -31,6 +40,37 @@ public class Launcher extends JFrame {
                 new App().startGame();
             }
         });
+        JButton btnOptions = new JButton("Options");
+        btnOptions.setBounds(width/2 - 40, height /2 - 40 + 30, 80, 40);
+        btnOptions.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new SettingsLauncher("Launcher - Options");
+            }
+        });
         window.add(btnPlay);
+        window.add(btnOptions);
+    }
+}
+
+class SettingsLauncher extends Launcher{
+    public SettingsLauncher(String title) {
+        super (title, 850, 700);
+        setSize(new Dimension(width, height));
+        setLocationRelativeTo(null);
+    }
+    @Override
+    protected void createComponents(){
+        JButton btnOK = new JButton("OK");
+        btnOK.setBounds(width - 100 , height - 70, 80, 30);
+        btnOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new Launcher("Game Launcher", 500, 500);
+            }
+        });
+        window.add(btnOK);
     }
 }
